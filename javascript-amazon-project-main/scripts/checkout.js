421b33b3187fb782f88eb5d390dd72e8bfebed19
Abhi0517 <abhigyantomar345@gmail.com>
2024-12-28 16:07:58 +0530

    Learning Promises

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,5 +2,12 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import "../data/backend-practice.js"
 
+//Runs inner function immediately.
+new Promise((resolve) => {
+    console.log('promise');
+}).then(() => {
+    console.log('next step');
+})
+
 renderOrderSummary();
-renderPaymentSummary();
+renderPaymentSummary(); 
